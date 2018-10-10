e1a90dcc3b3b738c339b2c9553623f3293752588
Alexander Hafstad <alexander@hafstad.dk>
2018-10-10 19:30:22 +0200

    doc(exercise): Added solution comments

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,18 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Card} from "./card";
 @Component({
   selector: 'itu-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  flipped = true;
   shield = 'url("../../assets/shield.svg")';
-  image = '../../../../assets/viking_happy.svg';
-  name = 'Happy viking';
-
-  // Exercise: Pass data into card component and use it in the template
-  // card.component.ts - Add input for card with an interface corresponding to the deck items
-  // card.component.ts - Remove unused properties - image and name
-  // card.component.html - replace name and image with the passed in data
-  // app.html - pass in data as "card" using property binding at itu-card
+  @Input() card: Card;
 }
