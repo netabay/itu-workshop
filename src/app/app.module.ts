975b7dfc9f695eddc473a0d706e5e8f3632e6f61
Alexander Hafstad <alexander@hafstad.dk>
2018-10-10 23:51:47 +0200

    fix(game, background): Fixed path so project is able to run at stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-// @ts-ignore
-import {BackgroundComponent} from 'background';
+import {BackgroundComponent} from "../../projects/background/src/lib/background.component";
 import {TitleComponent} from './title/title.component';
 import {CardComponent} from "./card/card.component";
 
